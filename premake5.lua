4d1320fc8ac407c3c22a3544e103687819d80878
lovely_santa <laurens.scheldeman@gmail.com>
2019-06-23 16:32:48 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+
+    -- Make sure the variables bin_dir and bin_int_dir are set!
+    targetdir (bin_dir)
+    objdir (bin_int_dir)
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
